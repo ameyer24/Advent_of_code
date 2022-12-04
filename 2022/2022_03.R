c00c25cdb1311a5682dbea88ab43a978395790f8
Andy Meyer <ameyer24@gmail.com>
2022-12-03 21:52:48 -0600

    Code is "cleaner" but I think some of the thinking is more opaque and the logic is less clear. Sometimes less is more and sometimes more is more? IDK.

--- 2022/2022_03.R
+++ 2022/2022_03.R
@@ -5,42 +5,31 @@ input <- read_csv("2022/inputs/2022_03.txt",
 # Need to split the string in half.
 
 compartments <- input %>%
-  mutate(string_len = str_length(items)) %>%
-  mutate(com1 = str_sub(items, 0, (string_len/2))) %>%
-  mutate(com2 = str_sub(items, -(string_len/2), -1))
-
-# The strings are split.
-# Find the matching character in each string.
-
-compartments1 <- compartments %>%
-  mutate(com1_split = str_split(com1, "")) %>%
-  mutate(com2_split = str_split(com2, "")) %>%
+  # subset the string in half into new columns
+  mutate(com1 = str_sub(items, 0, (str_length(items)/2))) %>%
+  mutate(com2 = str_sub(items, -(str_length(items)/2), -1)) %>%
+  # split each string into list of characters.
+  mutate(com1 = str_split(com1, "")) %>%
+  mutate(com2 = str_split(com2, "")) %>%
+  # look at each row and determine where the lists intersect
   rowwise() %>%
-  mutate(pair = intersect(com1_split,com2_split)) %>%
-  select(-com1_split,-com2_split) # just delete this garbage.
+  mutate(pair = intersect(com1,com2)) %>%
+  # calculate the score (using some constants build into R)
+  mutate(score = match(pair, c(letters, LETTERS)))
 
-# Got the match!
-# Now need to calculate the score
-# Found out that R has a list of all the letters!
-# combine those lists into one big list
-all_letters <- c(letters, LETTERS)
-# then the score is the position of the letter on that list.
-score <- compartments1 %>%
-  mutate(score = match(pair, all_letters))
-
-sum(score$score)
+# Find the sum of the score
+sum(compartments$score)
 
 # Part 2 ------------------------------------------------------------------
 # Start with the input again.
 # Need to look at groups of three; but how to create groups of three?
+# I don't know. But I can...
+# get the data I need and then filter out what I don't need.
 group_input <- input %>%
-  mutate(items_2 = lead(items,1)) %>%
-  mutate(items_3 = lead(items,2)) %>%
   rename(items_1 = items) %>%
-  mutate(row = row_number()) %>%
-  filter(row %% 3 == 1) %>%
-  select(-row) %>%
-  mutate(elf_group = row_number())
+  mutate(items_2 = lead(items_1,1)) %>%
+  mutate(items_3 = lead(items_1,2)) %>%
+  filter(row_number() %% 3 == 1)
 
 # Each elf group is in a row.
 group_input2 <- group_input %>%
@@ -54,6 +43,6 @@ group_input2 <- group_input %>%
   mutate(pair23 = list(intersect(items_2,items_3))) %>%
   mutate(match123 = list(intersect(pair12, pair23))) %>%
   select(-pair12, -pair23) %>% # Delete this garbage
-  mutate(score = match(match123, all_letters))
+  mutate(score = match(match123, c(letters, LETTERS)))
 
 sum(group_input2$score)
