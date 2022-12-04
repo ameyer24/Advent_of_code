7e9a745b268e6831c472bf123751a7613937b0c2
Andy Meyer <ameyer24@gmail.com>
2022-12-04 00:10:09 -0600

    Improve the approach greatly. Figured out how to filter on multiple conditions.

--- 2022/2022_04.R
+++ 2022/2022_04.R
@@ -5,52 +5,23 @@ input <- read_csv("2022/inputs/2022_04.txt",
 
 # Break apart the numbers
 # get when assignment 1 contains assignment 2
-input2 <- input %>%
+input <- input %>%
   separate(assignment1, into = c("start1","end1"), sep = "-") %>%
   separate(assignment2, into = c("start2","end2"), sep = "-") %>%
-  mutate(id = row_number()) %>%
-  type_convert() # DAMN IT
+  type_convert() # Need this! Forgot it and it botched me up.
 
+fully_contains <- input %>%
+  filter(start1 <= start2 & end1 >= end2 | start2 <= start1 & end2 >= end1)
 
-# case 1, when the first set includes all the second set
-# start 1 is <= start 2 / end1 is >=
-case1 <- input2 %>%
-  filter(start1 < start2) %>%
-  filter(end1 > end2)
-
-case1_1 <- input2 %>%
-  filter(start1 == start2) %>%
-  filter(end1 > end2)
-
-case1_2 <- input2 %>%
-  filter(start1 < start2) %>%
-  filter(end1 == end2)
-# case 2, when the second set includes all the first set
-# start 1 is >= start2 / end1 is <= end 2
-case2 <- input2 %>%
-  filter(start2 < start1) %>%
-  filter(end2 > end1)
-
-case2_1 <- input2 %>%
-  filter(start2 == start1) %>%
-  filter(end2 > end1)
-
-case2_2 <- input2 %>%
-  filter(start2 < start1) %>%
-  filter(end2 == end1)
-
-# case 3, when they are identical.
-# these are counted twice (in both cases above)
-case3 <- input2 %>%
-  filter(start1 == start2) %>%
-  filter(end1 == end2)
-
+nrow(fully_contains)
 
 # Part 2 ------------------------------------------------------------------
+# I approach this indirectly
+# Find where there is absolutely no overlap
+no_overlap <- input %>%
+  filter(end1 < start2 | end2 < start1)
+# and then subtract from the total
+any_overlap <- nrow(input) - nrow(no_overlap)
 
-# find where there is no overlap at all
 
-case90 <- input2 %>%
-  filter(end1 < start2)
-case91 <- input2 %>%
-  filter(end2 < start1)
+
